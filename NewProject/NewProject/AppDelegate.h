16fe335954cbe153bb237b55acfcfdf0114a1b49
Jianying Sun <jianying.sun@netviewtech.com>
2015-11-17 18:43:58 +0800

    no message

---
+++ NewProject/NewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewProject
+//
+//  Created by Jianying Sun on 15/11/17.
+//  Copyright © 2015年 Netviewtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
